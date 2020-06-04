112be84f2b6adc165eb51a7f87a11a34dfaff2c3
duffercn <duffercn@gmail.com>
2020-06-04 22:11:49 +0800

    Update config.php

--- web/config.php
+++ web/config.php
@@ -1,7 +1,7 @@
 <?php
 $gl=(isset($_COOKIE['gl']) && $_COOKIE['gl'])?$_COOKIE['gl']:'US';
 define('ROOT_PART', Root_part());
-define('APIKEY', 'AIzaSyBmYQPhWRuVcFG8XNO-WDBFLZpZtIkKlbY');
+define('APIKEY', 'AIzaSyDUNlgEgo46nP8ZRbhLwFOsTVX0p5nieHE');
 define('GJ_CODE', $gl);
 define('SITE_NAME', 'Hello World!');
 define('TITLENAME', 'Hello World!');
